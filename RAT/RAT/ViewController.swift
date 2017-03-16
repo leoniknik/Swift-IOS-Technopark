6595118fbd056dc38fd960e94c9f4a5a70c4f46e
Kirill <kirill@Kirills-Mac.local>
2017-03-16 15:07:22 -0700

    first commit

---
+++ RAT/RAT/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RAT
+//
+//  Created by Kirill on 3/16/17.
+//  Copyright © 2017 RAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
