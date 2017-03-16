43d3c4945921e6fc1ae24a3755277b6d02c5a88d
Kirill <leoniknik@mail.ru>
2017-03-16 16:39:35 -0700

    Creation design of autorization's viewcontroller

--- RAT/RAT/ViewController.swift
+++ RAT/RAT/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var imageLabelView: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var logInButton: UIButton!
+    
+    @IBOutlet weak var registrationLabel: UILabel!
+    
+    @IBOutlet weak var registrationButton: UIButton!
+    
+    @IBOutlet weak var vkLogInButton: UIButton!
+    
+    @IBOutlet weak var facebookLogInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
