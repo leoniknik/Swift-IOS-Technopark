1a52761212b1baac5cf99dbb9a9fc439d125cfd8
Kirill <leoniknik@mail.ru>
2017-03-24 06:37:14 -0700

    add Validator and prototype of POST request

--- RAT/RAT/RegistrationViewController.swift
+++ RAT/RAT/RegistrationViewController.swift
@@ -10,6 +10,50 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    @IBOutlet weak var firstnameTextField: UITextField!
+    @IBOutlet weak var lastnameTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    @IBAction func signUp(_ sender: Any) {
+        createRequest()
+    }
+    
+    func createRequest() -> Void {
+        // создаем сессию
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        // Выходим если не удалось инициировать УРЛ из строки
+        guard let URL = NSURL(string: "https://.../api/signup") else {return}
+        let request = NSMutableURLRequest(url: URL as URL)
+        // Формируем запрос для метода POST
+        request.httpMethod = "POST"
+        //  Устанавливаем заголовки запроса
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        // Само тело запроса
+        let bodyObject = [
+            "email":  "\(emailTextField.text)",
+            "password": "\(passwordTextField.text)",
+            "firstname": "\(firstnameTextField.text)",
+            "lastname": "\(lastnameTextField.text)",
+            "phone": "\(phoneTextField.text)"
+        ]
+        // Устанавливаем тело в запросе
+        request.httpBody = try! JSONSerialization.data(withJSONObject: bodyObject, options: [])
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            if (error == nil) {
+                
+            } else {
+                // Обработка ошибки коннекта
+                print("URL Session Task Failed: %@", error!.localizedDescription);
+            }
+        })
+        task.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ RAT/RAT/ValidatorHelper.swift
@@ -0,0 +1,38 @@
+//
+//  ValidatorHelper.swift
+//  RAT
+//
+//  Created by Kirill on 3/24/17.
+//  Copyright © 2017 RAT. All rights reserved.
+//
+
+import UIKit
+
+class ValidatorHelper: NSObject {
+    
+    class func isPasswordCorrect(_ value:String) -> Bool {
+        
+        let passwordRegExp = "[A-Za-z0-9]{8,24}"
+        let passwordTest = NSPredicate(format:"SELF MATCHES %@", passwordRegExp)
+        return passwordTest.evaluate(with: value)
+        
+    }
+    
+    class func isEmailCorrect(_ value:String) -> Bool {
+        
+        let emailRegExp = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegExp)
+        return emailTest.evaluate(with: value)
+        
+    }
+    
+    class func isPhoneCorrect(_ value:String) -> Bool {
+        
+        let phoneRegExp = "[0-9]{11}"
+        let phoneTest = NSPredicate(format:"SELF MATCHES %@", phoneRegExp)
+        return phoneTest.evaluate(with: value)
+        
+    }
+    
+    
+}
