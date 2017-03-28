fe80aeaa3bef88fb976feca49b4df4b6b2571756
Kirill <kirill@Kirills-Mac.local>
2017-03-28 12:40:50 -0700

    did signup request

--- RAT/RAT/RegistrationViewController.swift
+++ RAT/RAT/RegistrationViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class RegistrationViewController: UIViewController {
 
@@ -16,6 +18,7 @@ class RegistrationViewController: UIViewController {
     @IBOutlet weak var firstnameTextField: UITextField!
     @IBOutlet weak var lastnameTextField: UITextField!
     @IBOutlet weak var phoneTextField: UITextField!
+    
     
     @IBAction func signUp(_ sender: Any) {
         let email = emailTextField.text
@@ -28,6 +31,34 @@ class RegistrationViewController: UIViewController {
     }
     
     func createSignUpRequest(email: String, password: String, firstname: String, lastname: String, phone: String) -> Void {
+        
+        let SIGNUP_URL = "http://192.168.1.51:8000/api/signup"
+/*
+        let parameters: Parameters = [
+            "email": email,
+            "password": password,
+            "firstname": firstname,
+            "lastname": lastname,
+            "phone": phone
+        ]
+*/
+        let parameters: Parameters = [
+            "email": "aaaa@mail.ru",
+            "password": "kial",
+            "firstname": "aaa",
+            "lastname": "bbbb",
+            "phone": "123"
+        ]
+        
+        Alamofire.request(SIGNUP_URL, method: .post, parameters: parameters ).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
     override func viewDidLoad() {
