acfe9a6e68a8da67ed316c50727ece09fbac399e
leoniknik <leoniknik@mail.ru>
2017-03-30 13:20:36 -0700

    add Just

---
+++ RAT/RAT/Review.swift
@@ -0,0 +1,13 @@
+//
+//  Review.swift
+//  RAT
+//
+//  Created by Kirill on 3/29/17.
+//  Copyright © 2017 RAT. All rights reserved.
+//
+
+import Foundation
+
+class Review {
+    
+}
---
+++ RAT/RAT/Service.swift
@@ -0,0 +1,13 @@
+//
+//  Service.swift
+//  RAT
+//
+//  Created by Kirill on 3/29/17.
+//  Copyright © 2017 RAT. All rights reserved.
+//
+
+import Foundation
+
+class Service {
+   
+}
